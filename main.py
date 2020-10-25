0bcece7d14eb932a69e5414c9da055cfd7e7d320
Matej Delincak <55759748+mateju25@users.noreply.github.com>
2020-10-25 10:20:00 +0100

    Minor fixes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import heapq
+import os
+import profile
 import time
 
 # vyska a sirka hlavolamu
@@ -24,16 +26,19 @@ class Node:
     # metoda, podla ktorej heap porovnava stavy (najskor usporaduva podla suctu heuristiky a vzdialenosti a ak su
     # rovnake tak este ich usporiada na zaklade heuristiky)
     def __lt__(self, other):
-        if (self.heuristic + self.distance) != (other.heuristic + other.distance):
-            return (self.heuristic + self.distance) < (other.heuristic + other.distance)
-        else:
+        if (self.heuristic + self.distance) == (other.heuristic + other.distance):
             return self.heuristic < other.heuristic
+        else:
+            return (self.heuristic + self.distance) < (other.heuristic + other.distance)
 
 
+
+# region funkcie k heuristikam
 # vyrata pocet inverzii v stave
 def num_of_inversions(p_state):
     stack = []
-    sum = 0
+    res_sum = 0
+    global lines, column
     # vyrobim si z dvojrozmerneho pola jednorozmerne
     for i_l in range(0, lines):
         for j_c in range(0, column):
@@ -43,30 +48,31 @@ def num_of_inversions(p_state):
     # zratam inverzie
     for i in range(0, len(stack)):
         for j in range(i + 1, len(stack)):
+            if stack[i] == 0 or stack[j] == 0:
+                continue
             if stack[i] > stack[j]:
-                sum += 1
-    return sum
+                res_sum += 1
+    return res_sum
 
 
 # zisti, ci dany stav sa da usporiadat na stav (1 2 3 ...)
 def is_possible(p_state):
-    sum = num_of_inversions(p_state)
-    global column
-    global lines
+    tmp_sum = num_of_inversions(p_state)
+    global lines, column
     # sirka je parna
     if column % 2 == 1:
         # inverzie su parne
-        if sum % 2 == 0:
+        if tmp_sum % 2 == 0:
             return True
         else:
             return False
     else:
         line, col = find_blank(p_state)
         # riadok prazdneho miesta zospodu je neparny a inverzie su parne
-        if lines - line % 2 == 1 and sum % 2 == 0:
+        if lines - line % 2 == 1 and tmp_sum % 2 == 0:
             return True
         # riadok prazdneho miesta zospodu je parny a inverzie su neparne
-        elif lines - line % 2 == 0 and sum % 2 == 1:
+        elif lines - line % 2 == 0 and tmp_sum % 2 == 1:
             return True
         else:
             return False
@@ -79,13 +85,14 @@ def choose_heuristic(p_start_state, p_final_state):
         return heuristic_1(p_start_state, p_final_state)
     elif type_heu == 2:
         return heuristic_2(p_start_state, p_final_state)
+    elif type_heu == 3:
+        return heuristic_1(p_start_state, p_final_state) + heuristic_2(p_start_state, p_final_state)
 
 
 # heuristika 1 - pocet policok na nespravnom mieste
 def heuristic_1(p_start_state, p_final_state):
     heu = 0
-    global lines
-    global column
+    global lines, column
     for i_l in range(0, lines):
         for j_c in range(0, column):
             if p_start_state[i_l][j_c] != p_final_state[i_l][j_c]:
@@ -95,59 +102,95 @@ def heuristic_1(p_start_state, p_final_state):
 
 # najde poziciu danej hodnoty v stave (pouziva sa pri heuristike 2)
 def find(p_value, p_state):
+    global lines, column
     for i_l in range(0, lines):
         for j_c in range(0, column):
             if p_value == p_state[i_l][j_c]:
                 return i_l, j_c
+    return None
 
 
 # heuristika 2 - sucet vzdialenosti hodnot od svojho miesta
 def heuristic_2(p_start_state, p_final_state):
     heu = 0
-    global lines
-    global column
+    global lines, column
     for i_l in range(0, lines):
         for j_c in range(0, column):
             x_l, y_c = find(p_start_state[i_l][j_c], p_final_state)
-            heu += abs(i_l-x_l) + abs(j_c-y_c)
+            heu += abs(i_l - x_l) + abs(j_c - y_c)
     return heu
 
 
+# endregion
+
+
+# region funkcie k nacitavaniu vstupu
 # nacita sekvenciu cisel a m do 2d pola
 def create_state_from_string(p_input):
-    global lines
-    global column
+    global lines, column
     res = []
     x = list(p_input.split())
     for i_l in range(0, lines):
         res.append([])
         for j_c in range(0, column):
             if x[i_l * column + j_c] == 'm':
-                res[i_l].append(-1)
+                res[i_l].append(0)
             else:
                 res[i_l].append(int(x[i_l * column + j_c]))
     return res
 
 
+# zisti ci subor existuje
+def checks_existency():
+    filename = input("Názov súboru: ")
+    while not os.path.exists(filename):
+        print("Súbor neexistuje.")
+        filename = input("Názov súboru: ")
+    return filename
+
+
 # nacita vstup zo suboru
-def load_states_from_file(p_filename):
-    global lines
-    global column
-    file = open(p_filename, "r").read().split('\n')
+def load_states_from_file(filename):
+    global lines, column
+    file = open(filename, "r").read().split('\n')
+    if not file[0].isdigit():
+        return '1', '1'
     lines = int(file[0])
+    if not file[1].isdigit():
+        return '1', '1'
     column = int(file[1])
-    start = file[2]
-    final = file[3]
-    return create_state_from_string(start), create_state_from_string(final)
+    return file[2], file[3]
 
 
+# zisti ci je vstup dobry
+def is_input_correct(p_input):
+    saved_input = p_input
+    p_input = list(p_input.split())
+    global lines, column
+    if len(p_input) != lines * column:
+        return False
+    blank = False
+    state = create_state_from_string(saved_input)
+    for i in range(0, len(p_input)):
+        if p_input[i] == 'm':
+            blank = True
+    # hladam vsetky hodnoty ktore tam maju byt
+    for i in range(1, len(p_input)):
+        if find(i, state) is None:
+            return False
+    return blank
+
+
+# endregion
+
+
+# region A* algoritmus a dolezite funkcie
 # najde prazdne miesto v stave
 def find_blank(p_state):
-    global lines
-    global column
+    global lines, column
     for i_l in range(0, lines):
         for j_c in range(0, column):
-            if p_state[i_l][j_c] == -1:
+            if p_state[i_l][j_c] == 0:
                 return i_l, j_c
 
 
@@ -168,9 +211,9 @@ def print_process(p_node):
     steps.insert(0, p_node)
 
     # zistujem rozdiel a vypisem operatory
-    for x in range(0, len(steps)-1):
+    for x in range(0, len(steps) - 1):
         x1, y1 = find_blank(steps[x].state)
-        x2, y2 = find_blank(steps[x+1].state)
+        x2, y2 = find_blank(steps[x + 1].state)
         if x1 - 1 == x2:
             print("DOLE, ", end='')
         if x1 + 1 == x2:
@@ -179,57 +222,59 @@ def print_process(p_node):
             print("VPRAVO, ", end='')
         if y1 + 1 == y2:
             print("VĽAVO, ", end='')
-        #steps[x].print_state()
     print()
 
 
 # A* algoritmus - vytvorim si min_heap, kde ukladam vytvorene uzly, vyberiem vzdy najmensi prvok a rozbalim ho do
 # dalsich stavov, vsetky vytvorene stavy pridam do hash tabulky, aby som sa necyklil
+@profile
 def find_final(p_start_pos, p_final_pos):
     heap = []
-    first = Node(heuristic_1(p_start_pos, p_final_pos), 0, p_start_pos, None)
+    first = Node(choose_heuristic(p_start_pos, p_final_pos), 0, p_start_pos, None)
     created = {}
     while first.heuristic != 0:
         x_l, y_c = find_blank(first.state)
-
         # right shift
         if y_c - 1 >= 0:
             new = [list(x) for x in first.state]
             swap(x_l, y_c - 1, x_l, y_c, new)
-            if created.get(hash(str(new))) != new:
-                created[hash(str(new))] = new
-                heapq.heappush(heap, Node(heuristic_2(new, p_final_pos), first.distance + 1, new, first))
+            if created.get(str(new)) is None:
+                created[str(new)] = 1
+                heapq.heappush(heap, Node(choose_heuristic(new, p_final_pos), first.distance + 1, new, first))
 
         # left shift
         if y_c + 1 < column:
             new = [list(x) for x in first.state]
             swap(x_l, y_c + 1, x_l, y_c, new)
-            if created.get(hash(str(new))) != new:
-                created[hash(str(new))] = new
-                heapq.heappush(heap, Node(heuristic_2(new, p_final_pos), first.distance + 1, new, first))
+            if created.get(str(new)) is None:
+                created[str(new)] = 1
+                heapq.heappush(heap, Node(choose_heuristic(new, p_final_pos), first.distance + 1, new, first))
 
         # up shift
         if x_l + 1 < lines:
             new = [list(x) for x in first.state]
             swap(x_l + 1, y_c, x_l, y_c, new)
-            if created.get(hash(str(new))) != new:
-                created[hash(str(new))] = new
-                heapq.heappush(heap, Node(heuristic_2(new, p_final_pos), first.distance + 1, new, first))
+            if created.get(str(new)) is None:
+                created[str(new)] = 1
+                heapq.heappush(heap, Node(choose_heuristic(new, p_final_pos), first.distance + 1, new, first))
 
         # down shift
         if x_l - 1 >= 0:
             new = [list(x) for x in first.state]
             swap(x_l - 1, y_c, x_l, y_c, new)
-            if created.get(hash(str(new))) != new:
-                created[hash(str(new))] = new
-                heapq.heappush(heap, Node(heuristic_2(new, p_final_pos), first.distance + 1, new, first))
+            if created.get(str(new)) is None:
+                created[str(new)] = 1
+                heapq.heappush(heap, Node(choose_heuristic(new, p_final_pos), first.distance + 1, new, first))
 
         first = heapq.heappop(heap)
     return first
 
 
+# endregion
+
+
 # vypise menu a ovladanie programu
-def menu():
+def print_menu():
     global lines, column, type_heu
     print()
     print("*****************************************************************************")
@@ -237,45 +282,83 @@ def menu():
     print("                           Autor: Matej Delinčák                             ")
     print("*****************************************************************************")
     print()
-    file = input("Nacitat zo suboru? y/n: ")
+    go = True
+    file_name = ''
+    file = input("Nacitat zo suboru? y / iné: ")
     if file == 'y':
-        filename = input("Názov súboru: ")
+        file_name = checks_existency()
     else:
-        menu = input("Chceš riešiť 8-hlavolam? y/ine: ")
+        menu = input("Chceš riešiť 8-hlavolam? y / iné: ")
         if menu != 'y':
-            lines = int(input("Zadaj počet riadkov: "))
-            column = int(input("Zadaj počet stĺpcov: "))
-
-    print("Vyber si heuristiku: ")
-    print("1. Počet políčok, ktoré nie sú na svojom mieste")
-    print("2. Súčet vzdialeností jednotlivých políčok od ich cieľovej pozície")
-    type_heu = input("Možnosť: ")
-    menu = int(input("Koľko razy chceš zopakovať riešenie? Zadaj číslo: "))
+            lines = input("Zadaj počet riadkov: ")
+            while (not lines.isdigit()) or (int(lines) < 1):
+                print("Chybný vstup.")
+                lines = input("Zadaj počet riadkov: ")
+            lines = int(lines)
+            column = input("Zadaj počet stĺpcov: ")
+            while (not column.isdigit()) or (int(column) < 1):
+                print("Chybný vstup.")
+                column = input("Zadaj počet stĺpcov: ")
+            column = int(column)
 
     if file == 'y':
-        start_pos, final_pos = load_states_from_file(filename)
+        start_pos, final_pos = load_states_from_file(file_name)
+        if not (is_input_correct(start_pos) and is_input_correct(final_pos)):
+            print("Chybný vstup zo súboru.")
+            go = False
+        if go:
+            start_pos = create_state_from_string(start_pos)
+            final_pos = create_state_from_string(final_pos)
     else:
-        print("Zadaj", column * lines - 1, "čísel a m pre prázdne políčko: ", end='')
-        start_pos = create_state_from_string(input())
-        print("Zadaj", column * lines - 1, "čísel a m pre prázdne políčko: ", end='')
-        final_pos = create_state_from_string(input())
-
+        print()
+        print("Štartovacie rozloženie: Zadaj", column * lines - 1, "čísel a m pre prázdne políčko: ", end='')
+        start_pos = input()
+        while not is_input_correct(start_pos):
+            print("Chybný vstup.")
+            print("Štartovacie rozloženie: Zadaj", column * lines - 1, "čísel a m pre prázdne políčko: ", end='')
+            start_pos = input()
+        start_pos = create_state_from_string(start_pos)
+        print()
+        print("Koncové rozloženie: Zadaj", column * lines - 1, "čísel a m pre prázdne políčko: ", end='')
+        final_pos = input()
+        while not is_input_correct(final_pos):
+            print("Chybný vstup.")
+            print("Koncové rozloženie: Zadaj", column * lines - 1, "čísel a m pre prázdne políčko: ", end='')
+            final_pos = input()
+        final_pos = create_state_from_string(final_pos)
+        print()
     timer = 0.0
 
-    if is_possible(start_pos) == is_possible(final_pos):
-        print("Riešenie je možné.")
-        for i in range(0, menu):
-            start = time.time()
-            if menu > 1:
-                find_final(start_pos, final_pos)
-            else:
-                print("Postupnosť: ")
-                print_process(find_final(start_pos, final_pos))
-            end = time.time()
-            timer += end - start
-        print("Cas: ", timer / menu)
-    else:
-        print("Riešenie nie je možné.")
+    if go:
+        print("Vyber si heuristiku: ")
+        print("1. Počet políčok, ktoré nie sú na svojom mieste")
+        print("2. Súčet vzdialeností jednotlivých políčok od ich cieľovej pozície")
+        print("3. Súčet prvej a druhej heuristiky")
+        type_heu = input("Možnosť: ")
+        while (not type_heu.isdigit()) or (int(type_heu) < 1 or int(type_heu) > 3):
+            print("Chybný vstup.")
+            type_heu = input("Možnosť: ")
+        type_heu = int(type_heu)
+        menu = input("Koľko razy chceš zopakovať riešenie? Zadaj číslo: ")
+        while (not menu.isdigit()) or (int(menu) < 1):
+            print("Chybný vstup.")
+            menu = input("Koľko razy chceš zopakovať riešenie? Zadaj číslo: ")
+        menu = int(menu)
+        print()
+        if is_possible(start_pos) == is_possible(final_pos):
+            print("Riešenie je možné.")
+            for i in range(0, menu):
+                start = time.time()
+                if menu > 1:
+                    find_final(start_pos, final_pos)
+                else:
+                    print("Postupnosť: ")
+                    print_process(find_final(start_pos, final_pos))
+                end = time.time()
+                timer += end - start
+            print("Cas: ", timer / menu)
+        else:
+            print("Riešenie nie je možné.")
 
 
-menu()
+print_menu()
